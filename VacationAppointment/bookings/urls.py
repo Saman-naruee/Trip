4dc9c47799f47e68c8b035a6b3569c5b38957651
Saman-naruee <samannaruee@gmail.com>
2024-08-03 23:15:33 +0330

    add urls.py and project urls.py. and fill views method base and make html files

--- VacationAppointment/bookings/urls.py
+++ VacationAppointment/bookings/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
+from .views import make_trip, trip_update, delete_trip, list_trips
 
 urlpatterns = [
-    path('', views.make_trip, name='make_trip'),
+    path('trips/new/', views.make_trip, name='make_trip'),
+    path('trips/', views.trip_update, name='trip_update'),
+    path('trips/<int:pk>/delete/', views.delete_trip, name='delete_trip'),
+    path('trips/<int:pk>/edit/', views.trip_update, name='trip_update'),
+
 ]
+
+# /trips/new/: URL برای ویوی ایجاد سفر.
+# /trips/: URL برای ویوی لیست سفرها.
+# /trips/<int:pk>/edit/: URL برای ویوی به‌روزرسانی سفر.
+# /trips/<int:pk>/delete/: URL برای ویوی حذف سفر.
--- VacationAppointment/bookings/views.py
+++ VacationAppointment/bookings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from .forms import TripForm
-
+from django.urls import reverse_lazy
+from .models import Trip
 
 def make_trip(request):
     context = {
@@ -10,22 +11,40 @@ def make_trip(request):
         form = TripForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('trip_success')
+            return redirect('list_trips')
     else:
         form = TripForm()
+    return render(request, 'bookings/create_trip.html', context)
+
+
+def list_trips(request):
+    context = {
+        'trips' : Trip.objects.all(),
+    }
+    path = 'bookings/list_trips.html'
+    return render(request, path, context)
+
+
+def trip_update(request, id):
+    path = 'bookings/update_trip.html' 
+    trip = get_object_or_404(Trip, id = id)
+    if request.method == 'POST':
+        form = TripForm(request.POST, instance=trip)
+        if form.is_valid():
+            form.save()
+            return redirect(list_trips)
+    else:
+        form = TripForm(instance=trip)
+    context = {
+        'form' : form,
+    }
+    return render(request, path, context)
     
-    return render(request, 'templates/bookings/maketrip.html', context)
 
-def trip_list(request):
-    pass
-
-class TripUpdate():
-    pass
-
-class DeleteTrip():
-    pass
-
-
-
-# git checkout feature-branch
-# git pull origin feature-branch
+def delete_trip(request, id):
+    path = 'bookings/confirm_delete.html' 
+    trip = get_object_or_404(Trip, id = id)
+    if request.method == 'POST':
+        trip.delete()
+        return redirect(list_trips)
+    return render(request, path, context={'trip', 'trip'})
